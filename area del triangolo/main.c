5051adf2e1b0f8f513e68ce9b829c0d7fb3ca141
Lucio <lucioborelli94@gmail.com>
2024-09-22 13:31:01 +0200

    commit

---
+++ area del triangolo/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+float b,h,a;
+    printf("area triangolo:\n base: ");
+    scanf("%f",&b);
+    printf("altezza triangolo: ");
+ scanf("%f",&h);
+    a = h*b/2.0;
+    printf("l'area del triangolo e': %f\n ",a);
+}
---
+++ media/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float a,b,c;
+    printf("inserire il primo numero: ");
+    scanf("%f", &a);
+    printf("inserire il secondo numero: ");
+    scanf("%f",&b);
+    c=(a+b)/2.0;
+    printf("la media è: %f ",c);
+}
+9
